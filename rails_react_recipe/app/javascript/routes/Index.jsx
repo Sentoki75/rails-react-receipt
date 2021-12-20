454a9538023dce006eec64b0f7ea1a9ac189be4b
Jesus Garcia Lahoz <jesus.garcialahoz@sage.com>
2021-12-20 01:37:34 +0000

    Recipes react

--- rails_react_recipe/app/javascript/routes/Index.jsx
+++ rails_react_recipe/app/javascript/routes/Index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../components/Home";
+import Recipe from "../components/Recipes";
 
 export default (
   <Router>
     <Routes>
       <Route path="/" exact element={<Home/>} />
+      <Route path="/recipes" exact element={<Recipe/>} />
     </Routes>
   </Router>
 );
